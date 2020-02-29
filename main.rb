6c1cfb15f21f414d2f33dfc82577d70926a3e22c
Ronaldo Simão De Carvalho <PUCMINAS\1051291@PPC01INFLL00217.pucminas.net>
2020-02-28 22:09:59 -0300

    #1: Implementacao condiçao.

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
-
-numero_secreto= 121
-index = 0
-while index < 3 do
+palpite = 0
+numero_secreto = 121
+3.times do
+    if palpite != numero_secreto
+        
     puts "Entre com um palpite:"
     palpite= gets.chomp.to_i
     puts"Você acertou?"
     puts palpite == numero_secreto
-    index = index + 1
+    end
+    
 end
 
 
