4c1b39d43ed6a18dea7e80e40ae17c5431f10413
Ronaldo Simão De Carvalho <PUCMINAS\1051291@PPC01INFLL00132.pucminas.net>
2020-03-05 21:39:56 -0300

    Jogo revisado

--- main.rb
+++ main.rb
@@ -1,13 +1,56 @@
-
-numero_secreto= 121
-index = 0
-while index < 3 do
-    puts "Entre com um palpite:"
-    palpite= gets.chomp.to_i
-    puts"Você acertou?"
-    puts palpite == numero_secreto
-    index = index + 1
+def da_boas_vindas
+    puts "Bem-vindo ao jogo da adivinhacao"
+    puts "Qual e o seu nome?"
+    nome = gets
+    puts "\n\n\n\n"
+    puts "Comecaremos o jogo para voce, #{nome}"
 end
 
+def sorteia_numero_secreto
+    puts "Escolhendo um numero secreto entre 0 e 200..."
+    numero_secreto = 121 
+    puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
+    numero_secreto
+end
+def pede_um_numero(tentativa, limite_de_tentativas)
+    puts "\n\n\n"
+    puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
+    puts "Entre com o numero"
+    chute = gets
+    puts "Sera que acertou? Voce chutou #{chute}"
+    chute.to_i
+end
 
-
+def verifica_se_acertou(numero_secreto, chute)
+acertou = numero_secreto == chute 
+if acertou
+    puts "Acertou!"
+    return true
+else
+    maior = numero_secreto > chute 
+    if maior 
+        puts "O numero secreto e maior!"
+    else
+        puts "O numero secreto e menor!"
+    end
+    false
+    end
+end 
+def joga(limite_de_tentativas)
+    numero_secreto = sorteia_numero_secreto
+    (1..limite_de_tentativas).each do |tentativa|
+        chute = pede_um_numero tentativa, limite_de_tentativas
+        break if verifica_se_acertou numero_secreto, chute 
+    end
+end 
+def nao_quer_jogar? 
+    puts "Deseja jogar novamente? (S/N)"
+    quero_jogar = gets.strip.upcase
+    quero_jogar.casecmp('N').zero?
+end
+da_boas_vindas
+limite_de_tentativas = 3
+loop do
+    joga limite_de_tentativas
+    break if nao_quer_jogar?
+end
